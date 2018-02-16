049831a00e348bff90eff4432db9959467386b3d
Thomas Lenz <thomas.lenz96@gmail.com>
2018-02-16 13:34:45 -0600

    First commit on dev branch; This will eventually have everything (up until I commit something more noteworthy than a checkpoint) squashed into it

--- src/game/deck.py
+++ src/game/deck.py
@@ -25,12 +25,15 @@
 
  Where CARD_NAME is the number of the card, or title for face cards and aces
  Suit is the name of the suit, in all caps, as defined statically below.
+
+ @NOTE I'm currently making the card class, among other things which will be split into different files later. I'm doing it here for my convenience, so try to avoid importing these until that's done.
  '''
 
 from enum import Enum, unique
+import sys, copy, os, math
 
 @unique
-class Deck(Enum):
+class CardIDs(Enum):
     # Spades
     ACE_OF_SPADES = 1;
     TWO_OF_SPADES = 2;
@@ -115,3 +118,112 @@ class Suits(Enum):
     CLUBS = 1;
     HEARTS = 2;
     DIAMONDS = 3;
+
+class Card(int idNum):
+    int __cardID = 0; # Must be init'd, or it'll be super inconsistent on calls. Uses the CardIDs enum above. Wraps on overflow, not on underflow
+    
+    __init__(self):
+        if(idNum > 52):
+            __cardID = idNum % 52;
+        else if(idNum < 0):
+            print("ERROR: Requested card ID is less than 0; exiting");
+            sys.exit(0101);        
+
+
+
+class Deck(int cardsPerDeck, int deckCount):
+
+    # Varible declaration; Default to a single 52 card deck
+    int __numCardsPerDeck = 52;
+    int __numDecks = 1;
+
+    Card __cards[] = {};
+
+    def __init__(self, cardsPerDeck, deckCount):
+            __setCardsPerDeck(cardsPerDeck);
+            __setNumDecks(deckCount);
+
+            for(int dIndex = 0; i < __numDecks; index++): # For each deck, add 52 cards to the deck array
+              for(int cIndex = 0; i < __numCardsPerDeck; index++):
+                    __cards.append(Card(index+1);
+
+            
+
+    def getCardsPerDeck(self):
+        return self.__numCardsPerDeck;
+
+    def getNumDecks(self):
+        return self.__numDecks;
+
+    def getCards(self):
+        return self.__cards;
+
+    # ONLY use this for __init__
+    def __setCardsPerDeck(self, int cardNum):
+        if(cardNum == None): # This is for __init__, to clean it up. Basically does what we would do in init. This needs to be first so we can save a few cycles checking if the values are None twice per change
+            return;
+
+        if(cardNum > 52 || cardNum < 1):
+            printf("Invalid value for cards per deck, %d. Not going to touch it", cardNum);
+            return;
+
+        self.__cardsPerDeck = cardNum;
+        return;
+
+    def __setNumDecks(self, int deckCount):
+        if(deckCount == None): # Same reason as above
+            return;
+
+        if(deckCount < 1):
+            print("Invalid value for number of decks, %d. Not going to touch it", deckCount);
+            return;
+            
+        self.__numDecks = deckCount;
+        return;
+
+    def dealCard(self, Card discard[]):
+        '''
+        Clone the __cards array, subtract the discard pile, RNG a value
+        Also, we can overload this with no arguments to just RNG a card out of the array
+
+        I'm leaving that second line in, but fuck python for not allowing overloading
+        '''
+
+        Card cardsDup[] = copy.deepcopy(self.getCards());
+        if(discard != None):
+            cardsDup -= discard;
+
+        Card rngCard = cardsDup[os.urandom % cardsDup.len()];
+
+        return rngCard;
+
+    def dealCards(self, int numCards, Card discardPile[]):
+        '''
+        Return an array of numCards random cards. Use a discard array to avoid dup's
+        Using this, we can deal out ((2*numPlayers)+5) cards in order, and classify them after
+        '''
+        int cardsDelt[] = {};
+        int discard[] = discardPile; # So we can affect the discard pile appropriately
+        int cardsDup[] = copy.deepcopy(self.getCards());
+
+        for(int index = 0; index < numCards; index++):
+            # Remove all the already used cards
+            cardsDup -= discard;
+            
+            '''
+            I don't want to use os.random since I don't know how many numbers need to be generated,
+            and don't want the program to lock up waiting in an uninterruptable sleep waiting for
+            entropy that won't be generated. If I notice issues with RNG (which I almost certainly won't, cuz human), I'll change it
+
+            I also want to use the cardsDelt length instead of anything using the cards per deck and number of decks
+            since it'll be ugly to incorporate all that and the discard array, which I'd need to call the size of anyway.
+            So why do the extra calcs? Verification isn't super important, given the inconsistent nature of the sising of arrays.
+            '''
+            Card rngCard = cardsDup[os.urandom % cardsDelt.len()]; # Generate a random card; Doing this here so I can add it to the discard pile as well 
+            cardsDelt.append(rngCard);
+            discard.append(rngCard);
+
+        return cardsDelt; # No need to do anything w/ discard; Because of how it was declared locally, all changes will be made; Effecitvely passed by reference
+
+    def resetDeck(self):
+        self.__init__(None,None);
