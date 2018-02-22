db989e3453637f10ac89c0d197ad3e49ebaa2302
Thomas Lenz <thomas.lenz96@gmail.com>
2018-02-22 13:26:22 -0600

    Seperated the Card and Deck files Prep'd to add odds calculation Prep'd to add checks on whether or not we have a given hand Added a few notes

---
+++ src/agent/AgentHandScanner.py
@@ -0,0 +1,36 @@
+'''
+@author Thomas Lenz <thomas.lenz96@gmail.com> AS The2b
+@date 22 February 2018
+@project Texas Hold'em AI
+@file AgentHandScanner.py
+
+This is where I'm going to keep the functions to see what we have in our hand
+'''
+
+from deck import Card
+
+class AgentHandScanner:
+
+    '''
+    Checks your hand and the board to see if we have a pair in our hand.
+    Does **NOT** calculate odds. It does, however, verify there are enough cards to form a hand before it checks anything.
+
+    @param Card cards[]
+    @return boolean hasPair
+    '''
+    def checkPair(self, cards):
+
+    def checkTwoPair(self, cards):
+
+    def checkThreeOfAKind(self, cards):
+
+    def checkStraight(self, cards):
+
+    def checkFlush(self, cards):
+
+    def checkFullHouse(self, cards):
+
+    def checkFourOfAKind(self, cards):
+
+    def checkStraightFlush(self, cards):
+
--- src/game/deck.py
+++ src/game/deck.py
@@ -1,142 +1,24 @@
 #!/usr/bin/python3
 
-# @author Thomas Lenz <thomas.lenz96@gmail.com> AS The2b
-# @date 15 Feb 2018
-# @project AI-Texas-Holdem
-# @file deck.py
+'''
+@author Thomas Lenz <thomas.lenz96@gmail.com> AS The2b
+@date 15 Feb 2018
+@project Texas Hold'em AI
+@file deck.py
+@ErrorID 2XX
+'''
 
 '''
- The purpose of this file is to enumerate the deck and manage a discard pile, as well as add methods for things such as dealing.
- This may change to just be an enumeration file, and have a seperate controller class
- I'm going to do it by suit since that's easier for me to conceptualize, but that may also change later
- 
- As it stands, each value can be accessed with the following algorithm:
-    (SuitMult * 13) + Value
-
- With Value being the value of the card (Jack = 11, Queen = 12, King = 13, Ace = 1)
- SuitMult:
-    Spades = 0
-    Clubs = 1
-    Hearts = 2
-    Diamonds = 3
-
- They can also be accessed by their name, in the form
-    <CARD_NAME>_OF_<SUIT>
-
- Where CARD_NAME is the number of the card, or title for face cards and aces
- Suit is the name of the suit, in all caps, as defined statically below.
-
- @TODO @NOTE I'm currently making the card class, among other things which will be split into different files later. I'm doing it here for my convenience, so try to avoid importing these until that's done.
- '''
+ The purpose of this class to deck management, and to give the deck functions for proper access.
+ In effect, this is a portal to the cards, which are all stored in a list.
+'''
 
 from enum import Enum, unique # Duh
 import sys # exit
 import copy # deepcopy
 import random # randint
 import operator # sub
-
-@unique
-class CardIDs(Enum):
-    # Spades
-    ACE_OF_SPADES = 1;
-    TWO_OF_SPADES = 2;
-    THREE_OF_SPADES = 3;
-    FOUR_OF_SPADES = 4;
-    FIVE_OF_SPADES = 5;
-    SIX_OF_SPADES = 6;
-    SEVEN_OF_SPADES = 7;
-    EIGHT_OF_SPADES = 8;
-    NINE_OF_SPADES = 9;
-    TEN_OF_SPADES = 10;
-    JACK_OF_SPADES = 11;
-    QUEEN_OF_SPADES = 12;
-    KING_OF_SPADES = 13;
-
-    # Clubs
-    ACE_OF_CLUBS = 14;
-    TWO_OF_CLUBS = 15;
-    THREE_OF_CLUBS = 16;
-    FOUR_OF_CLUBS = 17;
-    FIVE_OF_CLUBS = 18;
-    SIX_OF_CLUBS = 19;
-    SEVEN_OF_CLUBS = 20;
-    EIGHT_OF_CLUBS = 21;
-    NINE_OF_CLUBS = 22;
-    TEN_OF_CLUBS = 23;
-    JACK_OF_CLUBS = 24;
-    QUEEN_OF_CLUBS = 25;
-    KING_OF_CLUBS = 26;
-
-    # Hearts
-    ACE_OF_HEARTS = 27;
-    TWO_OF_HEARTS = 28;
-    THREE_OF_HEARTS = 29;
-    FOUR_OF_HEARTS = 30;
-    FIVE_OF_HEARTS = 31;
-    SIX_OF_HEARTS = 32;
-    SEVEN_OF_HEARTS = 33;
-    EIGHT_OF_HEARTS = 34;
-    NINE_OF_HEARTS = 35;
-    TEN_OF_HEARTS = 36;
-    JACK_OF_HEARTS = 37;
-    QUEEN_OF_HEARTS = 38;
-    KING_OF_HEARTS = 39;
-
-    # Diamonds
-    ACE_OF_DIAMONDS = 40;
-    TWO_OF_DIAMONDS = 41;
-    THREE_OF_DIAMONDS = 42;
-    FOUR_OF_DIAMONDS = 43;
-    FIVE_OF_DIAMONDS = 44;
-    SIX_OF_DIAMONDS = 45;
-    SEVEN_OF_DIAMONDS = 46;
-    EIGHT_OF_DIAMONDS = 47;
-    NINE_OF_DIAMONDS = 48;
-    TEN_OF_DIAMONDS = 49;
-    JACK_OF_DIAMONDS = 50;
-    QUEEN_OF_DIAMONDS = 51;
-    KING_OF_DIAMONDS = 52;
-
-    '''Use this method to get the numerical value of a card
-
-    @param int card
-    @return int numValue
-    '''
-    def getCardNum(cardID):
-        # Modulo 13 to remove the suit multiplier
-        val = card % 13;
-        return val;
-
-    def getCardSuit(cardID):
-        # Store the modulo 13 and subtract it, removing the numerical value
-        numVal = card % 13;
-        suit = card - numVal;
-        # Then divide it, which will always be an int thanks to modulo, giving the suit multiplier
-        suit = suit / 13;
-        return Suits.suit;
-
-@unique
-class Suits(Enum):
-    SPADES = 0;
-    CLUBS = 1;
-    HEARTS = 2;
-    DIAMONDS = 3;
-
-class Card:
-    __cardID = 0; # Must be init'd, or it'll be super inconsistent on calls. Uses the CardIDs enum above. Wraps on overflow, not on underflow
-    
-    def __init__(self, idNum):
-        if(idNum > 52):
-            idNum = idNum % 52;
-        elif(idNum < 0):
-            print("ERROR: Requested card ID is less than 0; exiting"); # @TODO I may want to change this to check for less than 1 instead, since I don't have a card defined for 0 as is. It's just as bad as having a threshold at -1, really
-            sys.exit(101);        
-
-        self.__cardID = idNum;
-
-    def getCardID(self):
-        return self.__cardID;
-
+from card import Card # Duh. It shouldn't access any enumeration directly
 
 class Deck:
 
@@ -146,6 +28,13 @@ class Deck:
 
     __cards = [];
 
+    '''
+    Creates the Deck object and assigns the values required to define the proper number of cards and decks.
+    The handlers for null values are in the set option, and therefore can be ignored
+
+    @param int cardsPerDeck (optional)
+    @param int deckCount (optional)
+    '''
     def __init__(self, cardsPerDeck, deckCount):
             self.__setCardsPerDeck(cardsPerDeck);
             self.__setNumDecks(deckCount);
@@ -168,7 +57,11 @@ class Deck:
     def getCards(self):
         return self.__cards;
 
-    # Pull a list of all the card IDs in the deck
+    '''
+    Pull a list of all the card IDs in the deck
+
+    @return cardIDs[]
+    '''
     def getCardIDs(self):
         cardIDs = [];
         cardList = copy.deepcopy(self.getCards());
@@ -176,7 +69,15 @@ class Deck:
             cardIDs.append(cardList[index].getCardID());
         return cardIDs;
 
-    # ONLY use this for __init__
+    '''
+    Sets the number of cards per deck. If it's over 52, it'll modulo the value. TBH this is a week after I originally wrote this
+    and idk why I chose that to handle the error instead of just making it 52. But I'm sure I have a reason.
+
+    If it's under 1, it tells you and ignores the input, returning -10
+
+    ONLY use this for __init__
+    '''
+
     def __setCardsPerDeck(self, cardNum):
         if(cardNum == None): # This is for __init__, to clean it up. Basically does what we would do in init. This needs to be first so we can save a few cycles checking if the values are None twice per change
             return;
@@ -236,6 +137,10 @@ class Deck:
         Using this, we can deal out ((2*numPlayers)+5) cards in order, and classify them after
         '''
 
+        # Verify we're not asking for more than 52 cards, which is impossible.
+        if(numCard > 52):
+            print("Trying to deal more than 52 cards, which exceeds the limit. There's no reason to ask for this many, so something error'd out. Exiting...");
+            sys.exit(103);
         # If it's not defined, skip the next part
         if(discard != None):
             # Make sure we're not trying to deal more cards than we have in the deck
